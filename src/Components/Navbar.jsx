891e5c2a3d39fae2b9aeb8125230a4f4ad5d77aa
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2025-02-20 01:31:09 +0100

    Changed the navbar logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import nav from "../assets/NavImage/Nav1.png";
+import nav from "../assets/NavImage/NavIcon.png";
 import {Button} from "./ui/button.jsx";
 import {Menu, X} from "lucide-react";
 
@@ -29,7 +29,7 @@ function Navbar() {
         <header className="sticky top-0 left-0 right-0 z-20 bg-[#2C2F1E] bg-opacity-50 backdrop-blur-md">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center py-4">
-                    <div className="w-16 relative">
+                    <div className="w-20 relative">
                         <img src={nav}
                              alt="Logo"
                         />
